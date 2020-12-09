4dbdbe433d3bb969c7a583b41a8cb0dd044c44a6
Sam <samdravitzki@gmail.com>
2020-12-09 23:38:17 +1300

    Switched loopbounds to using mods still has problems tho
    - only works on y axis
    - only works in downwards direction on y axis
    - still jerks on loop

--- Assets/Scripts/Infinity/LoopWithinBounds.cs
+++ Assets/Scripts/Infinity/LoopWithinBounds.cs
@@ -20,55 +20,26 @@ namespace Infinity
         // Update is called once per frame
         void Update()
         {
-            var pos = transform.position;
-            if (pos.y >= LoopBounds.max.y)
-                transform.position = ToBottom(transform.position);
-            if (pos.y <= LoopBounds.min.y)
-                transform.position = ToTop(transform.position);
-            if (pos.z >= LoopBounds.max.z)
-                transform.position = ToBackward(transform.position);
-            if (pos.z <= LoopBounds.min.z)
-                transform.position = ToForward(transform.position);
-            if (pos.x >= LoopBounds.max.x)
-                transform.position = ToLeft(transform.position);
-            if (pos.x <= LoopBounds.min.x)
-                transform.position = ToRight(transform.position);
+            // TODO: Problems 1. Only works in one direction (Downwards looping, but not up), 2. Still get jerking
+            var y = transform.position.y;
+            var loopedy = ((y-LoopBounds.extents.y) % -LoopBounds.size.y) + LoopBounds.extents.y;
+            // var loopedPos = (transform.position - LoopBounds.extents).Mod(-LoopBounds.size) + LoopBounds.extents;
+            transform.position = new Vector3(transform.position.x, loopedy, transform.position.z);
         }
-
-        Vector3 ToTop(Vector3 pos)
-        {
-            return new Vector3(pos.x, LoopBounds.max.y, pos.z); // + Vector3.down;
-        }
-
-        Vector3 ToBottom(Vector3 pos)
-        {
-            return new Vector3(pos.x, LoopBounds.min.y, pos.z); // + Vector3.up;
-        }
-
-        Vector3 ToForward(Vector3 pos)
-        {
-            return new Vector3(pos.x, pos.y, LoopBounds.max.z); // + Vector3.back;
-        }
-
-        Vector3 ToBackward(Vector3 pos)
-        {
-            return new Vector3(pos.x, pos.y, LoopBounds.min.z); // + Vector3.forward;
-        }
-
-        Vector3 ToRight(Vector3 pos)
-        {
-            return new Vector3(LoopBounds.max.x, pos.y, pos.z); // + Vector3.left;
-        }
-
-        Vector3 ToLeft(Vector3 pos)
-        {
-            return new Vector3(LoopBounds.min.x, pos.y, pos.z); // + Vector3.right;
-        }
-
+        
         private void OnDrawGizmos()
         {
-            Gizmos.color = new Color(0, 1, 0);
+            Gizmos.color = new Color(1, 0, 0);
             Gizmos.DrawWireCube(LoopBounds.center, new Vector3(LoopBounds.extents.x * 2, LoopBounds.extents.y * 2, LoopBounds.extents.z * 2));
         }
     }
 }
+
+static class Vector3Extensions
+{
+    public static Vector3 Mod(this Vector3 v, Vector3 mod)
+    {
+        return new Vector3(v.x % mod.x, v.y % mod.y, v.z % mod.z); 
+    }
+}
+
