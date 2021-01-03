b416beb687d0264cf7955848251c5f4e6dd44930
Sam <samdravitzki@gmail.com>
2021-01-03 16:46:06 +1300

    Added FlipClamp operation to looping
    Simple way to loop the character within bounds

---
+++ Assets/Scripts/Infinity/InfinityUntilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Infinity
+{
+    static class InifinityUtilities
+    {
+        // Flip clamp is a flipped version of a clamp operation
+        // Where the where when a value reaches max it is set to min and vice versa
+        // Standard clamp operation for reference:
+        // var t = val < min ? min : val;
+        // return t > max ? max : t;
+        public static float FlipClamp(float val, float min, float max) {
+            var t = val < min ? max : val;
+            return t > max ? min : t;
+        }
+    }
+    
+}
--- Assets/Scripts/Infinity/LoopWithinBounds.cs
+++ Assets/Scripts/Infinity/LoopWithinBounds.cs
@@ -20,26 +20,20 @@ namespace Infinity
         // Update is called once per frame
         void Update()
         {
-            // TODO: Problems 1. Only works in one direction (Downwards looping, but not up), 2. Still get jerking
-            var y = transform.position.y;
-            var loopedy = ((y-LoopBounds.extents.y) % -LoopBounds.size.y) + LoopBounds.extents.y;
-            // var loopedPos = (transform.position - LoopBounds.extents).Mod(-LoopBounds.size) + LoopBounds.extents;
-            transform.position = new Vector3(transform.position.x, loopedy, transform.position.z);
+            var pos = transform.position;
+            var loopedPos = new Vector3(
+                InifinityUtilities.FlipClamp(pos.x, LoopBounds.min.x, LoopBounds.max.x),
+                InifinityUtilities.FlipClamp(pos.y, LoopBounds.min.y, LoopBounds.max.y),
+                InifinityUtilities.FlipClamp(pos.z, LoopBounds.min.z, LoopBounds.max.z)
+                );
+            transform.position = loopedPos;
         }
-        
+
         private void OnDrawGizmos()
         {
             Gizmos.color = new Color(1, 0, 0);
             Gizmos.DrawWireCube(LoopBounds.center, new Vector3(LoopBounds.extents.x * 2, LoopBounds.extents.y * 2, LoopBounds.extents.z * 2));
         }
-    }
-}
-
-static class Vector3Extensions
-{
-    public static Vector3 Mod(this Vector3 v, Vector3 mod)
-    {
-        return new Vector3(v.x % mod.x, v.y % mod.y, v.z % mod.z); 
     }
 }
 
